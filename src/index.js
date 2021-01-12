8caecb34032042152694b07ae00015af2b54f0a5
Renee <renee13wi@gmail.com>
2021-01-12 16:45:35 +0900

    Add greeting.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import counter from "./component/counter";
 import TodoList from "./component/TodoList";
 import toggleClick from "./component/toggleClick";
 import clock from "./component/clock";
+import Greeting from "./component/Greeting";
 
 // counter
 counter();
@@ -10,3 +11,4 @@ const todoWrap = document.querySelector(".todo-wrap");
 new TodoList(todoWrap);
 toggleClick();
 clock();
+Greeting();
